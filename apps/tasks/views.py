f3df044f913c251498019ff2ff23362bb2704030
Erzhan Zhusupov <seytek@Noutbuk-Erzhan.local>
2025-02-23 02:30:59 -0600

    Auth

--- apps/tasks/views.py
+++ apps/tasks/views.py
@@ -2,23 +2,34 @@ from django.shortcuts import render, redirect
 from .models import Task
 from datetime import datetime
 from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+
 
 
 def get_tasks(request):
-    tasks = Task.objects.all()
-    return render(request, 'cards.html', {'tasks': tasks})
+    if request.user.is_authenticated:
+        tasks = Task.objects.filter(owner=request.user)
+        return render(request, 'cards.html', {'tasks': tasks})
+    else:
+        return render(request, 'cards.html', {'is_authenticated': False}) 
 
 
 def tasks(request):
-    if request.method == "POST":
-        content = request.POST.get('content')
-        description = request.POST.get('description')
-        time_str = request.POST.get('time')
-        time = datetime.strptime(time_str, '%Y-%m-%dT%H:%M')
-        Task.objects.create(title=content, description=description, created_at=time)
-        return redirect('/')
-    tasks = Task.objects.all()
-    return render(request, 'cards.html', {'tasks': tasks})
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            content = request.POST.get('content')
+            description = request.POST.get('description')
+            time_str = request.POST.get('time')
+            time = datetime.strptime(time_str, '%Y-%m-%dT%H:%M')
+            task_owner=request.user
+            Task.objects.create(title=content, description=description, created_at=time, owner=task_owner)
+            return redirect('/')
+        tasks = Task.objects.filter(owner=request.user)
+        return render(request, 'cards.html', {'tasks': tasks})
+    else:
+        return render(request, 'cards.html', {'is_authenticated': False})
+        
+    
 
 
 def delete_task(request,task_id):
